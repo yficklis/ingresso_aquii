e9827ff362ae851eaa898b72e58dd398b70aee6d
Yficklis <yficklis.santos@gmail.com>
2024-02-29 05:50:29 -0300

    first structure

--- lib/pages/shopping_cart.dart
+++ lib/pages/shopping_cart.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -25,13 +26,107 @@ class _ShoppingCartState extends State<ShoppingCart> {
     return Provider.of<Shop>(context, listen: false).formatTotalPrice();
   }
 
+  Future<String> getIdMovieTheatherByName(String cineName, String type) async {
+    try {
+      CollectionReference cinemaRef =
+          FirebaseFirestore.instance.collection('cinemas');
+      QuerySnapshot cinemasSnapshot = await cinemaRef
+          .where('unidade', isEqualTo: cineName)
+          .where('tipo', isEqualTo: type)
+          .where('status', isEqualTo: true)
+          .get();
+
+      return (cinemasSnapshot.docs.isNotEmpty)
+          ? cinemasSnapshot.docs.first.id
+          : '';
+    } on FirebaseAuthException catch (e) {
+      if (e.code != '') {
+        // Log or handle the FirebaseAuthException
+        print('FirebaseAuthException: ${e.code}');
+        return '';
+      }
+    } catch (e) {
+      // Log or handle other exceptions
+      print('Error: $e');
+      return '';
+    }
+    return '';
+  }
+
+  Future<QuerySnapshot> retrieveAllLotesByCinemaId(String cinemaId) async {
+    // Reference to the "lotes" subcollection
+    CollectionReference lotesRef = FirebaseFirestore.instance
+        .collection('cinemas')
+        .doc(cinemaId)
+        .collection('lotes');
+
+    // Retrieve all "lotes" of the cinema
+    QuerySnapshot lotesSnapshot =
+        await lotesRef.where('status', isEqualTo: true).get();
+    return lotesSnapshot;
+  }
+
+  retriveSubCollectionByLote(
+    String cinemaId,
+    String loteId,
+    String itemCollection,
+    int limit,
+  ) async {
+    // Reference to the "item" subcollection inside the current "lote"
+    CollectionReference itemRef = FirebaseFirestore.instance
+        .collection('cinemas')
+        .doc(cinemaId)
+        .collection('lotes')
+        .doc(loteId)
+        .collection(itemCollection);
+
+    // Limit the number of "ingresso" documents to 5
+    Query itemQuery = itemRef
+        .where('status', isEqualTo: true)
+        .where('vendido', isEqualTo: false)
+        .limit(limit);
+
+    // Retrieve all "item" documents inside the current "lote"
+    QuerySnapshot itemSnapshot = await itemQuery.get();
+
+    // Process each "item" document
+    for (QueryDocumentSnapshot itemDocument in itemSnapshot.docs) {
+      // Access the "item" document ID
+      String itemId = itemDocument.id;
+
+      // Update the "vendido" attribute to false
+      await itemRef.doc(itemId).update({'vendido': true});
+
+      // Create a subcollection inside the "user" collection to store the "item" IDs
+      CollectionReference userTicketsRef = FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .collection('my_tickets');
+
+      // Add the "item" ID to the "my_tickets" subcollection inside
+      await userTicketsRef.add({
+        'itemId': itemId,
+        'type': itemCollection,
+        'status': true,
+      });
+    }
+  }
+
   // pay button tapped
-  void payNow() {
-    /*
-    
-      fill out your payment service here
-    
-     */
+  void payNow() async {
+    String cinemaId =
+        await getIdMovieTheatherByName('Praia Grande', 'regional');
+
+    // Retrieve all "lotes" of the cinema
+    QuerySnapshot lotesSnapshot =
+        await this.retrieveAllLotesByCinemaId(cinemaId);
+
+    // Iterate through each "lote" document
+    for (QueryDocumentSnapshot loteDocument in lotesSnapshot.docs) {
+      // Access the "lote" document ID
+      String loteId = loteDocument.id;
+      retriveSubCollectionByLote(cinemaId, loteId, 'combos', 2);
+    }
   }
 
   @override
